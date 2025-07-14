12467a04699420fae961d77dd38ce866986ead24
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-07-14 11:22:18 +0300

    login form added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scb_login_page/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,12 +7,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:
+      home:LoginPage()
     );
   }
 }
---
+++ lib/widgets/welcome_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WelcomeSection extends StatelessWidget {
+  const WelcomeSection({super.key, required Animation<Offset> slideAnimation, required Animation<double> fadeAnimation});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
