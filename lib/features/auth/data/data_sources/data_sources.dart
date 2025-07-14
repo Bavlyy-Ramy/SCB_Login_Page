83180fab9c1ec58126bfe0b060ed16cd1ca2d7b1
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-07-14 15:39:22 +0300

    created each layer files

---
+++ lib/features/auth/data/data_sources/data_sources.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DataSources extends StatelessWidget {
+  const DataSources({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/auth/data/repositories_impl/repositories_impl.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RepositoriesImpl extends StatelessWidget {
+  const RepositoriesImpl({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
